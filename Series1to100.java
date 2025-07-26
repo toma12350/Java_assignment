ac172b3d16d21de912cce103021b7c1ba720a477
toma12350 <summaiyazannat64@gmail.com>
2025-07-26 15:17:40 +0700

    Create Series1to100.java

---
+++ Series1to100.java
@@ -0,0 +1,10 @@
+public class Series1to100{
+    public static void main(String[] args){
+        int i;
+        System.out.println("1 to 100 :");
+        for(i=1;i<=100;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
