b426f4beccf3807a9ca47a52bf1e3ce94561edbb
toma12350 <summaiyazannat64@gmail.com>
2025-07-26 15:19:52 +0700

    Create SumSeries.java

---
+++ SumSeries.java
@@ -0,0 +1,11 @@
+public class SumSeries {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum of numbers from 1 to 100 is: " + sum);
+    }
+}
