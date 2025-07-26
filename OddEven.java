8878ca40768fb8a57bbf384c80bee357d7594160
toma12350 <summaiyazannat64@gmail.com>
2025-07-26 15:14:28 +0700

    Create OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,25 @@
+public class OddEven {
+    public static void main(String[]args){
+        int a=5;
+        int b=10;
+        //check for 'a'
+        if(a % 2 == 0)
+        {
+            System.out.println("a is an Even number");
+        }
+        else
+        {
+            System.out.println("a is an Odd number");
+        }
+        //check for 'b'
+        if(b % 2 == 0)
+        {
+            System.out.println("b is an Even number");
+        }
+        else
+        {
+            System.out.println("b is an Odd number");
+        }
+    }
+    
+}
